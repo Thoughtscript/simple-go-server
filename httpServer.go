4c814535d6624868bc9c721289179dcf511a9c10
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-09-13 11:41:58 -0500

    add more realistic business logic

--- httpServer.go
+++ httpServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	h "simplegoserver/handlers"
 	"time"
@@ -20,14 +21,47 @@ func main() {
 		var second time.Duration = 1000000000
 		var seconds time.Duration = 4
 
+		numbers := []int{
+			1,
+			2,
+			3,
+			4,
+			5,
+			15,
+		}
+
+		messages := []string{
+			"Example logging STDOUT",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+			"Beep Boop Beep Boop!",
+			"Another example logging STDOUT",
+			"Proin luctus quis velit nec congue.",
+			"Phasellus ac ligula pulvinar, viverra odio ultricies, mattis sapien.",
+		}
+
 		for {
 			fmt.Println("=================== TEST GO ROUTINE ===================")
-			fmt.Println("Example logging STDOUT")
-			fmt.Println("Lorem ipsum dolor sit amet, consectetur adipiscing elit...")
-			fmt.Println("Fizz")
-			fmt.Println("Buzz")
-			fmt.Println("Fizz Buzz")
-			fmt.Println("Example logging STDOUT")
+			fmt.Println()
+			fmt.Println("I can generate text just like a real AI too:")
+			fmt.Println(messages[rand.Intn(len(messages))])
+			fmt.Println(messages[rand.Intn(len(messages))])
+			fmt.Println()
+
+			fmt.Println("Performing critical business logics:")
+			myNum := numbers[rand.Intn(len(numbers))]
+			fmt.Println("Computing - what is: ", myNum)
+
+			if myNum%3 == 0 && myNum%5 == 0 {
+				fmt.Println(myNum, "is a FizzBuzz!")
+			} else if myNum%3 == 0 {
+				fmt.Println(myNum, "is a Fizz!")
+			} else if myNum%5 == 0 {
+				fmt.Println(myNum, "is a Buzz!")
+			} else {
+				fmt.Println(myNum, "is just a number :(")
+			}
+
+			fmt.Println()
 			time.Sleep(second * seconds)
 		}
 	}()
